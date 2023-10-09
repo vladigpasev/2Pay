3fe94d5d7595f0615c14b77701850b4005cd3316
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-10-09 22:43:01 +0300

    idk

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -75,7 +75,7 @@ export const Header = () => {
           </label>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-base-content"
           >
             <li>
               <a>Item 1</a>
@@ -116,7 +116,7 @@ export const Header = () => {
           <li tabIndex={0}>
             <details>
               <summary>Parent</summary>
-              <ul className="p-2">
+              <ul className="p-2 text-base-content">
                 <li>
                   <a>Submenu 1</a>
                 </li>
