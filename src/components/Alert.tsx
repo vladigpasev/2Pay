7084e5a5719580edb9e9382e1bb797e961f1a51b
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-10-24 11:33:15 +0300

    added Alert component

---
+++ src/components/Alert.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { atom, useAtom } from "jotai";
+
+export const alertAtom = atom({
+  opened: false,
+  content: <></>,
+});
+
+export default function AlertDialog() {
+  const [alert, setAlert] = useAtom(alertAtom);
+
+  return (
+    <>
+      <dialog
+        open={alert.opened}
+        id="my_modal_5"
+        className="modal fixed flex text-neutral-content modal-bottom sm:modal-middle w-[99vw] backdrop-blur-sm backdrop-contrast-50"
+      >
+        <div className="modal-box left-1/2 relative -translate-x-1/2 p-3 pt-0">
+          <div className="modal-action">{alert.content}</div>
+          <button
+            className="btn top-0 right-0 absolute rounded-tl-none rounded-br-none rounded-bl-xl"
+            type="button"
+            onClick={() => setAlert({ opened: false, content: alert.content })}
+          >
+            <FontAwesomeIcon icon={faClose} />
+          </button>
+        </div>
+      </dialog>
+    </>
+  );
+}
--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -5,25 +5,24 @@ import { Footer } from "./Footer";
 import { Header, themeAtom } from "./Header";
 import { Suspense } from "react";
 import PageLoading from "@/app/loading";
-import dynamic from "next/dynamic";
-import Notifyers from "./Notifyers";
+// import dynamic from "next/dynamic";
+// import Notifyers from "./Notifyers";
 
-const NotifyersWithNoSSR = dynamic(() => import("./Notifyers"), {
-  ssr: false,
-});
-
+// const NotifyersWithNoSSR = dynamic(() => import("./Notifyers"), {
+//   ssr: false,
+// });
 export const Wrapper = ({ children }: { children: React.ReactNode }) => {
   const [theme] = useAtom(themeAtom);
   return (
     <div data-theme={theme}>
-      <div className="w-full max-w-[1536px] relative left-1/2 -translate-x-1/2">
+      <div className="w-full">
         <Header />
         <div className="relative h-fit min-h-[calc(100vh-65px)]">
           <Suspense fallback={<PageLoading />}>{children}</Suspense>
         </div>
         <Footer />
       </div>
-      <NotifyersWithNoSSR />
+      {/* <NotifyersWithNoSSR /> */}
       {/* <Notifyers /> */}
     </div>
   );
