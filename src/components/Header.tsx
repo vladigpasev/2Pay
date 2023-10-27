c60d1988e0351a463521bc1c37e1b2e443ad114a
Aleks Nikolov <tcr.official.world@gmail.com>
2023-10-28 01:18:40 +0300

    header fixed, dropdown menu needs to be looked up by team

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,9 @@ import { useHydrateAtoms } from 'jotai/utils';
 import ThemeToggler from './ThemeToggler';
 import { useLogout } from '@/auth/logout';
 import { useUser } from '@/hooks/useUser';
+import logo from '../../public/images/branding/2pay-logo-green-short.png'
+import Image from 'next/image';
+
 
 export const lightTheme = 'paymentLight';
 export const darkTheme = 'paymentDark';
@@ -17,8 +20,9 @@ const LightDarkThemeSwitch = ({ cookies }: { cookies: Map<string, any> }) => {
 export const Header = ({ cookies }: { cookies: Map<string, any> }) => {
   const user = useUser();
 
+  //[hsl(var(--nf)/0.65)] -for bg color
   return (
-    <div className='navbar text-neutral-content top-0 sticky bg-[hsl(var(--nf)/0.65)] shadow-md shadow-[hsl(var(--b2)/0.25)] backdrop-blur-md z-50'>
+    <div className='navbar top-0 sticky bg-secondary shadow-md shadow-[hsl(var(--b2)/0.25)] backdrop-blur-md z-50 text-neutral-content'>
       <div className='navbar-start'>
         <div className='dropdown'>
           <label tabIndex={0} className='btn btn-ghost lg:hidden'>
@@ -34,52 +38,73 @@ export const Header = ({ cookies }: { cookies: Map<string, any> }) => {
           </label>
           <ul
             tabIndex={0}
-            className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-base-content'
+            className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-base-content text-'
           >
             <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
+              <a>For Businesses</a>
               <ul className='p-2'>
                 <li>
-                  <a>Submenu 1</a>
+                  <a>Contacts</a>
                 </li>
                 <li>
-                  <a>Submenu 2</a>
+                  <a>Items</a>
+                </li>
+                <li>
+                  <a>Sales</a>
                 </li>
               </ul>
             </li>
             <li>
-              <a>Item 3</a>
+              <a>For Clients</a>
+              <ul className='p-2'>
+                <li>
+                  <a>Contacts</a>
+                </li>
+                <li>
+                  <a>Purchases</a>
+                </li>
+                <li>
+                  <a>Payment</a>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
-        <Link href='/' className='btn btn-ghost normal-case text-xl text-secondary max-sm:-ml-5'>
-          <img className='h-7 sm:h-9' src='/images/branding/icon.png' alt='DoNuts' />
-          Do<strong className='text-primary p-0 -ml-1.5'>Nuts</strong>
-        </Link>
+        <Image src={logo} alt='' className='w-2/5 md:w-1/5'></Image>
       </div>
       <div className='navbar-center hidden lg:flex'>
         <ul className='menu menu-horizontal px-1'>
-          <li>
-            <a>Item 1</a>
-          </li>
           <li tabIndex={0}>
             <details>
-              <summary>Parent</summary>
+              <summary>For Business</summary>
               <ul className='p-2 text-base-content'>
                 <li>
-                  <a>Submenu 1</a>
+                  <a>Contacts</a>
                 </li>
                 <li>
-                  <a>Submenu 2</a>
+                  <a>Items</a>
+                </li>
+                <li>
+                  <a>Sales</a>
                 </li>
               </ul>
             </details>
           </li>
           <li>
-            <a>Item 3</a>
+            <details>
+              <summary>For Clients</summary>
+              <ul className='p-2 text-base-content'>
+                <li>
+                  <a>Contacts</a>
+                </li>
+                <li>
+                  <a>Purchases</a>
+                </li>
+                <li>
+                  <a>Payment</a>
+                </li>
+              </ul>
+            </details>
           </li>
         </ul>
       </div>
@@ -100,7 +125,7 @@ export const Header = ({ cookies }: { cookies: Map<string, any> }) => {
             <Link href='/auth/signin' className='btn btn-ghost ml-1 max-sm:ml-0 max-sm:p-1'>
               Sign In
             </Link>
-            <Link href='/auth/signup' className='btn btn-secondary max-sm:p-2'>
+            <Link href='/auth/signup' className='btn btn-primary max-sm:p-2'>
               Register
             </Link>
           </>
