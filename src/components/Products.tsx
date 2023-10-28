8ac38880dbcb6ff8d774a765ba8831199e126d57
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-10-28 11:09:56 +0300

    fixed valdis shit

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,100 +1,98 @@
-"use client"
+'use client';
 import React, { useState } from 'react';
 import Product from '@/components/Product';
 import Gallery from './Gallery';
 
 function Products() {
-    const productList = [
-        {
-            title: "Product 1",
-            description: "Description for product 1",
-            price: "$20",
-            mainImage: "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-            gallery: [
-                "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-                "https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0",
-                "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
-                "https://www.volusion.com/blog/content/images/2021/09/Product-Photography.jpeg"
-            ],
-            soldCount: 5 
-        },
-        {
-            title: "Product 1",
-            description: "Description for product 1",
-            price: "$20",
-            mainImage: "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-            gallery: [
-                "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-                "https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0",
-                "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
-                "https://www.volusion.com/blog/content/images/2021/09/Product-Photography.jpeg"
-            ],
-            soldCount: 5 
-        },
-        {
-            title: "Product 1",
-            description: "Description for product 1",
-            price: "$20",
-            mainImage: "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-            gallery: [
-                "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-                "https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0",
-                "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
-                "https://www.volusion.com/blog/content/images/2021/09/Product-Photography.jpeg"
-            ],
-            soldCount: 5 
-        },
-        {
-            title: "Product 1",
-            description: "Description for product 1",
-            price: "$20",
-            mainImage: "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-            gallery: [
-                "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-                "https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0",
-                "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
-                "https://www.volusion.com/blog/content/images/2021/09/Product-Photography.jpeg"
-            ],
-            soldCount: 5 
-        },
-        {
-            title: "Product 1",
-            description: "Description for product 1",
-            price: "$20",
-            mainImage: "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-            gallery: [
-                "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg",
-                "https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0",
-                "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
-                "https://www.volusion.com/blog/content/images/2021/09/Product-Photography.jpeg"
-            ],
-            soldCount: 5 
-        },
-        // ... other products
-    ];
+  const productList = [
+    {
+      title: 'Product 1',
+      description: 'Description for product 1',
+      price: '$20',
+      mainImage:
+        'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg',
+      gallery: [
+        'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg',
+        'https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0',
+        'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D',
+        'https://www.volusion.com/blog/content/images/2021/09/Product-Photography.jpeg'
+      ],
+      soldCount: 5
+    },
+    {
+      title: 'Product 1',
+      description: 'Description for product 1',
+      price: '$20',
+      mainImage:
+        'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg',
+      gallery: [
+        'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg',
+        'https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0',
+        'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D',
+        'https://www.volusion.com/blog/content/images/2021/09/Product-Photography.jpeg'
+      ],
+      soldCount: 5
+    },
+    {
+      title: 'Product 1',
+      description: 'Description for product 1',
+      price: '$20',
+      mainImage:
+        'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg',
+      gallery: [
+        'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg',
+        'https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0',
+        'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D',
+        'https://www.volusion.com/blog/content/images/2021/09/Product-Photography.jpeg'
+      ],
+      soldCount: 5
+    },
+    {
+      title: 'Product 1',
+      description: 'Description for product 1',
+      price: '$20',
+      mainImage:
+        'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg',
+      gallery: [
+        'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg',
+        'https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0',
+        'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D',
+        'https://www.volusion.com/blog/content/images/2021/09/Product-Photography.jpeg'
+      ],
+      soldCount: 5
+    },
+    {
+      title: 'Product 1',
+      description: 'Description for product 1',
+      price: '$20',
+      mainImage:
+        'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg',
+      gallery: [
+        'https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg',
+        'https://burst.shopifycdn.com/photos/wrist-watches.jpg?width=1000&format=pjpg&exif=0&iptc=0',
+        'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&q=80&w=1000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D',
+        'https://www.volusion.com/blog/content/images/2021/09/Product-Photography.jpeg'
+      ],
+      soldCount: 5
+    }
+    // ... other products
+  ];
 
-    const [activeGallery, setActiveGallery] = useState<string[] | null>(null);
+  const [activeGallery, setActiveGallery] = useState<string[] | null>(null);
 
-    const closeGallery = () => {
-        setActiveGallery(null);
-    };
+  const closeGallery = () => {
+    setActiveGallery(null);
+  };
 
-    return (
-        <div className="sm:max-h-[calc(100vh-64px)] p-10 space-y-6 sm:overflow-y-auto">
-            {productList.map((product, idx) => (
-                <Product key={idx} product={product} setActiveGallery={setActiveGallery} />
-            ))}
+  return (
+    <div className='sm:max-h-[calc(100vh-64px)] p-10 max-sm:p-0 space-y-6 sm:overflow-y-auto'>
+      {productList.map((product, idx) => (
+        <Product key={idx} product={product} setActiveGallery={setActiveGallery} />
+      ))}
 
-            {activeGallery && (
-                <Gallery 
-                    images={activeGallery}
-                    closeGallery={closeGallery}
-                />
-            )}
-        </div>
-    );
-
-
+      {activeGallery && <Gallery images={activeGallery} closeGallery={closeGallery} />}
+    </div>
+  );
 }
 
 export default Products;
