a50af68e730940adf6478adca37fa72083c6d8bf
Spasimir Pavlov <spasimir.pavlov@gmail.com>
2023-10-28 11:48:02 +0300

    Added product backend

--- db/schema.ts
+++ db/schema.ts
@@ -1,14 +1,4 @@
-import {
-  int,
-  timestamp,
-  mysqlTable,
-  primaryKey,
-  varchar,
-  mysqlEnum,
-  boolean,
-  float,
-  index
-} from 'drizzle-orm/mysql-core';
+import { int, timestamp, mysqlTable, varchar, mysqlEnum, boolean, float, json } from 'drizzle-orm/mysql-core';
 import { relations, sql } from 'drizzle-orm';
 
 export const authProviderEnum = mysqlEnum('authProvider', ['email', 'google', 'facebook']);
@@ -41,8 +31,10 @@ export const companies = mysqlTable('companies', {
     .primaryKey(),
   name: varchar('name', { length: 128 }).notNull(),
   contactEmail: varchar('contactEmail', { length: 256 }).notNull(),
-  logoURL: varchar('logoURL', { length: 256 }),
-  creatorUuid: varchar('creatorUuid', { length: 256 }).notNull()
+  description: varchar('description', { length: 2048 }).notNull(),
+  logoURL: varchar('logoURL', { length: 256 }).notNull(),
+  creatorUuid: varchar('creatorUuid', { length: 256 }).notNull(),
+  soldItems: int('soldItems').notNull().default(0)
 });
 
 export const products = mysqlTable('products', {
@@ -51,9 +43,12 @@ export const products = mysqlTable('products', {
     .notNull()
     .primaryKey(),
   name: varchar('name', { length: 128 }).notNull(),
+  stripeId: varchar('stripeId', { length: 64 }).notNull(),
   price: float('price').notNull(),
   description: varchar('description', { length: 2048 }).notNull(),
-  pictureURL: varchar('pictureURL', { length: 256 }),
+  pictureURL: varchar('pictureURL', { length: 256 }).notNull(),
+  galleryJSON: json('galleryJSON').$type<string[]>().notNull(),
+  amountSold: int('amountSold').notNull(),
   companyUuid: varchar('companyUuid', { length: 256 }).notNull()
 });
 
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import { authenticationRouter } from './routers/authenticationRouter';
+import { productRouter } from './routers/productRouter';
 import { stripeRouter } from './routers/stripeRouter';
 import { userRouter } from './routers/userRouter';
 import { t } from './trpc';
@@ -7,6 +8,8 @@ export const appRouter = t.router({
   authentication: authenticationRouter,
   user: userRouter,
   stripe: stripeRouter,
+  product: productRouter
 });
 
 export type AppRouter = typeof appRouter;
+
