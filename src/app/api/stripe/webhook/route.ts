5f284e7530c6e2122dd4aa4cc779f61297ce9d24
Spasimir Pavlov <spasimir.pavlov@gmail.com>
2023-10-28 20:04:44 +0300

    Stripe webhook

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -1,13 +1,12 @@
 import Stripe from 'stripe';
-import { headers } from 'next/headers'
-
+import { headers } from 'next/headers';
+import { buyProduct } from '@/server/service/products';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2023-10-16'
 });
 
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
-
 
 export async function POST(req: Request) {
   const payload = await req.text();
@@ -25,20 +24,17 @@ export async function POST(req: Request) {
     });
   }
 
-  // Handle the event
   switch (event.type) {
-    case 'invoice.payment_succeeded':
-      const invoicePaymentSucceeded = event.data.object;
-      // Then define and call a function to handle the event invoice.payment_succeeded
-      break;
-    // ... handle other event types
+    case 'checkout.session.completed':
+      const webhookData = event.data.object;
+      const metadata = webhookData.metadata as { stripeId: string; userId: string };
+      await buyProduct(metadata.userId, metadata.stripeId);
     default:
       console.log(`Unhandled event type ${event.type}`);
   }
 
-  console.log(`Webhook received: ${JSON.stringify(payload)}`);
-
   return new Response('Received', {
     status: 201
   });
-};
+}
+
