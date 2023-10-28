a7fcac9b88ba15653e6a23a2235dce25999f914f
Aleks Nikolov <tcr.official.world@gmail.com>
2023-10-28 19:12:25 +0300

    about page done (?)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,12 +3,23 @@ import payment from '../../../public/images/pngs/payment-phone.png'
 import banking from '../../../public/images/svg/Hybrid-Events-for-Financial-Services-Banking.svg'
 import crypto from '../../../public/images/pngs/crypto.png'
 import teamMember from '../../../public/images/pngs/team-member.png'
+import aboutTitle from '../../../public/images/pngs/about-payments.png'
 
 export default function About() {
+
     return (
         <>
+            <section className="about-top bg-gradient-to-r from-[#00a54c] to-[#01853e] flex items-center px-10 justify-around gap-6">
+                <Image src={aboutTitle} alt="" className="max-[768px]:hidden w-1/6"></Image>
+                <section className="text-neutral max-[768px]:text-center">
+                    <p className="text-7xl">About us.</p>
+                    <p className="text-xl">Here you can get to know more about who are we and what is our mission.</p>
+                </section>
+            </section>
+            <section className="location">
+            </section>
             <section className="title pb-32">
-                <p className="text-6xl py-8 text-center text-primary pt-12 pb-16">Who are we?</p>
+                <p className="text-5xl py-8 text-center text-primary pt-12 pb-16">Who are we?</p>
                 <section className="items flex flex-col min-[900px]:flex-row gap-20 items-center justify-center md:mx-28">
                     <section className="flex flex-col items-center justify-center">
                         <Image src={payment} alt='' className="w-64 min-[900px]:w-3/5"></Image>
@@ -27,8 +38,11 @@ export default function About() {
                     </section>
                 </section>
             </section>
+            <section className="highlight-text bg-neutral text-base min-[425px]:text-2xl md:text-3xl leading-7 mb-32 py-7 text-center px-20">
+                2Pay is one of the highest rising payment platforms right now! We deliver to our customers the most effective payment and e-commerce services in the region. But to make this possible, there is a team of dedicated developers, that stands behind 2Pay.
+            </section>
             <section className="team pb-12">
-                <p className="text-6xl text-primary text-center pb-16">Our team</p>
+                <p className="text-5xl text-primary text-center pb-16">Our team</p>
                 <section className="team-members flex flex-col min-[1280px]:flex-row gap-40 items-center justify-center md:mx-28">
                     <section className="flex flex-col items-center justify-center">
                         <Image src={teamMember} alt="" className="w-64"></Image>
