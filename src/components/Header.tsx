91df39a8927980ba5ae9934ebb4c5ba169fb2730
Nikola Andreev <76113098+nikolaandreev2008programmer@users.noreply.github.com>
2023-10-29 10:20:50 +0200

    fiixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,14 @@ import { useHydrateAtoms } from 'jotai/utils';
 import ThemeToggler from './ThemeToggler';
 import { useLogout } from '@/auth/logout';
 import { useUser } from '@/hooks/useUser';
-import logo from '../../public/images/branding/2pay-logo-green-short.png'
-import burger from '../../public/images/svg/menu-hamburger.svg'
+import logo from '../../public/images/branding/2pay-logo-green-short.png';
+import burger from '../../public/images/svg/menu-hamburger.svg';
 import Image from 'next/image';
 import { useState } from 'react';
 
 import { useLoadTokens } from '@/auth/token';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFire, faHome } from '@fortawesome/free-solid-svg-icons';
 
 export const Header = () => {
   useLoadTokens();
@@ -29,44 +31,38 @@ export const Header = () => {
   return (
     <div className='navbar top-0 sticky bg-secondary shadow-md shadow-[hsl(var(--b2)/0.25)] z-50 text-neutral-content'>
       <div className='navbar-start max-[400px]:-ml-2'>
-        <div className='dropdown backdrop-blur-sm'>
-          <label tabIndex={0} className='btn btn-ghost lg:hidden '>
-            <Image src={burger} alt='' className='w-12'></Image>
+        <div className='dropdown'>
+          <label tabIndex={0} className='btn btn-ghost lg:hidden'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h8m-8 6h16' />
+            </svg>
           </label>
-          <ul
-            tabIndex={0}
-            className='menu menu-sm flex flex-col backdrop-blur-sm dropdown-content mr-32 mt-3 z-[1] p-2 shadow shadow-black bg-base-100 rounded-box text-base-content'
-          >
-            <li className='flex flex-col w-[768px] '>
-              <Link href="/">For Businesses</Link>
-              <ul className='p-2'>
-                <li>
-                  <Link href="/">Contacts</Link>
-                </li>
-                <li>
-                  <Link href="/">Items</Link>
-                </li>
-                <li>
-                  <Link href="/">Sales</Link>
-                </li>
-              </ul>
+          <ul tabIndex={0} className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'>
+            <li>
+              <Link href={'/trending'}>
+                <strong className='font-extrabold'>
+                  <FontAwesomeIcon icon={faFire} className='mr-0.5' /> Trending
+                </strong>
+              </Link>
             </li>
             <li>
-              <Link href="/">For Clients</Link>
-              <ul className=''>
-                <li>
-                  <Link href="/">Contacts</Link>
-                </li>
-                <li>
-                  <Link href="/">Purchases</Link>
-                </li>
-                <li>
-                  <Link href="/">Payment</Link>
-                </li>
-              </ul>
+              <Link href={'/'}>
+                <strong className='font-extrabold'>
+                  <FontAwesomeIcon icon={faHome} className='mr-0.5' /> Home
+                </strong>
+              </Link>
             </li>
             <li>
-              <Link href="/">Explore</Link>
+              <Link href={'/about'}>About Us</Link>
+            </li>
+            <li>
+              <Link href={'/contact'}>Contact Us</Link>
             </li>
           </ul>
         </div>
@@ -76,37 +72,26 @@ export const Header = () => {
       </div>
       <div className='navbar-center hidden lg:flex'>
         <ul className='menu menu-horizontal max-[400px]:px-1'>
-          <li tabIndex={0}>
-            <details>
-              <summary>For Business</summary>
-              <ul className='p-2 text-base-content'>
-                <li>
-                  <Link href="/">Contacts</Link>
-                </li>
-                <li>
-                  <Link href="/">Items</Link>
-                </li>
-                <li>
-                  <Link href="/">Sales</Link>
-                </li>
-              </ul>
-            </details>
+          <li>
+            <Link href={'/trending'}>
+              <strong className='font-extrabold'>
+                <FontAwesomeIcon icon={faFire} className='mr-0.5' /> Trending
+              </strong>
+            </Link>
           </li>
           <li>
-            <details>
-              <summary>For Clients</summary>
-              <ul className='p-2 text-base-content'>
-                <li>
-                  <Link href="/">Contacts</Link>
-                </li>
-                <li>
-                  <Link href="/">Purchases</Link>
-                </li>
-                <li>
-                  <Link href="/">Payment</Link>
-                </li>
-              </ul>
-            </details>
+            <Link href={'/'}>
+              <strong className='font-extrabold'>
+                {' '}
+                <FontAwesomeIcon icon={faHome} className='mr-0.5' /> Home
+              </strong>
+            </Link>
+          </li>
+          <li>
+            <Link href={'/about'}>About Us</Link>
+          </li>
+          <li>
+            <Link href={'/contact'}>Contact Us</Link>
           </li>
         </ul>
       </div>
